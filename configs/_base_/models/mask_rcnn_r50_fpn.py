7f0438681556031877a4b943f4e7d101ad5b0dba
JJho1314 <1543748178@qq.com>
2023-03-03 15:11:08 +0800

    main

--- configs/_base_/models/mask_rcnn_r50_fpn.py
+++ configs/_base_/models/mask_rcnn_r50_fpn.py
@@ -36,14 +36,14 @@ model = dict(
             type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
         loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
     roi_head=dict(
-        type='StandardRoIHead',
+        type='StandardRoIHeadTEXT',
         bbox_roi_extractor=dict(
             type='SingleRoIExtractor',
             roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
             out_channels=256,
             featmap_strides=[4, 8, 16, 32]),
         bbox_head=dict(
-            type='Shared2FCBBoxHeadTEXT',
+            type='Shared2FCBBoxHead',
             in_channels=256,
             fc_out_channels=1024,
             roi_feat_size=7,
